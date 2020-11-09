415dba3565ae5b050974c19468923386c55ea26d
DidierFontaine <71550187+DidierFontaine@users.noreply.github.com>
2020-11-09 11:42:56 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log("Hello Winc Academy")
+            let name ='Didier';
+            console.log(name)
+            let number= 4
+            let number2= 4
+            console.log(number + number2)
+            console.log(number - number2)
+            console.log(number * number2)
+            let number3= "4"
+            let number4= "4"
+            console.log(number3 + number4) 
+            let age=26;
+            console.log(typeof age)
+            let age1="fourthy eight";
+            console.log(typeof age1)
